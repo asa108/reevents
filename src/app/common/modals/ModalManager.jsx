fee9c719aa3a96f400a660bd328fcb3a4fef85dd
Asane <yaradayo317@gmail.com>
2021-10-11 18:39:47 -0400

    added sign in modal

--- src/app/common/modals/ModalManager.jsx
+++ src/app/common/modals/ModalManager.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import TestModal from "../../../fetures/sandox/TestModal";
+import LoginForm from "../../../fetures/auth/LoginForm";
 
 export default function ModalManager() {
-  const modalLookup = { TestModal };
+  const modalLookup = { TestModal, LoginForm };
   const currentModal = useSelector((state) => state.modals);
   console.log("currentModal", currentModal);
   let renderedModal;
--- src/fetures/nav/SignedOutMenu.jsx
+++ src/fetures/nav/SignedOutMenu.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Menu, Button } from "semantic-ui-react";
+import { openModal } from "../../app/common/modals/modalReducer";
 
 export default function SignedOutMenu({ setAuth }) {
+  const dispatch = useDispatch();
   return (
     <Menu.Item position="right">
-      <Button onClick={() => setAuth(true)} basic inverted content="Login" />
+      <Button
+        onClick={() => dispatch(openModal({ modalType: "LoginForm" }))}
+        basic
+        inverted
+        content="Login"
+      />
       <Button
         basic
         inverted
